8e70b8842fcfc44ab560d30dd8a3412f4a243d84
florent-455 <70588283+florent-455@users.noreply.github.com>
2023-05-12 19:53:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true, 
+        prevArrow: '<img src="images/arrow-left.svg" alt="" class="arrow arrow-left">',
+        nextArrow: '<img src="images/arrow-right.svg" alt="" class="arrow arrow-right">',
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1,
+                    prevArrow: false,
+                    nextArrow: false,
+                }
+            }
+        ]
+    });
+});
